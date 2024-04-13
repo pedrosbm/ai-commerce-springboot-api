9bb376025897896a4048fbc92f67b0d92f4f1115
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-04-13 18:34:56 -0300

    teste do funcionamento do springboot

--- src/main/java/com/pedrosbm/aicommerce/AicommerceApplication.java
+++ src/main/java/com/pedrosbm/aicommerce/AicommerceApplication.java
@@ -2,10 +2,19 @@ package com.pedrosbm.aicommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class AicommerceApplication {
 
+	@GetMapping
+	@ResponseBody
+	public String teste(){
+		return "Spring boot funcionando";
+	} 
 	public static void main(String[] args) {
 		SpringApplication.run(AicommerceApplication.class, args);
 	}
