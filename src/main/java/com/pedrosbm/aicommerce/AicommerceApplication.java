53267d99565dc43103077893bc9c1c0f6779e637
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-04-13 15:53:29 -0300

    first commit, springboot base

---
+++ src/main/java/com/pedrosbm/aicommerce/AicommerceApplication.java
@@ -0,0 +1,13 @@
+package com.pedrosbm.aicommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrosbm/aicommerce/AicommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosbm.aicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
