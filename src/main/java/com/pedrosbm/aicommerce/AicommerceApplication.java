fd3a7ac48faa980f8156588fbfc8bc312aabe805
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-04-14 21:04:33 -0300

    pequeno teste

--- src/main/java/com/pedrosbm/aicommerce/AicommerceApplication.java
+++ src/main/java/com/pedrosbm/aicommerce/AicommerceApplication.java
@@ -2,10 +2,8 @@ package com.pedrosbm.aicommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 public class AicommerceApplication {
 
 	public static void main(String[] args) {
